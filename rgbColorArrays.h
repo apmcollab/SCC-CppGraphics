79302eb5401fc2c76628529d4448da7da1e9218f
Chris Anderson <cander@g.ucla.edu>
2023-11-14 08:20:33 -0800

    added rgbColorArrays

---
+++ rgbColorArrays.h
@@ -0,0 +1,20 @@
+//
+// RGB color arrays created using "I Want Hue" tool : 
+// https://medialab.github.io/iwanthue/
+//
+#ifndef RGB_COLOR_ARRAYS_
+#define RGB_COLOR_ARRAYS_
+class rgbColorArrays
+{
+public :
+
+static std::vector<std::vector<double>> 
+rgb20 =  
+{{38,47,0},  {103,0,1},    {1,48,158},   {97,0,161},   {0,96,45},
+{166,0,119}, {0,103,86},   {58,125,0},   {1,121,191},  {222,57,0},
+{255,0,63},  {128,143,0},  {255,60,131}, {195,168,0},  {0,209,83},
+{255,160,57},{213,169,255},{100,208,255},{124,218,147},{251,184,136}};
+};
+#endif
+
+
