b37bd3ce216e7620bd36709b2260c5619756ed1b
Chris Anderson <cander@g.ucla.edu>
2025-04-28 08:14:09 -0700

    added gpl header

--- CAMgraphics.cpp
+++ CAMgraphics.cpp
@@ -12,6 +12,28 @@
 #include "SVGdriver.h"
 #include "UCdriver_ps.h"
 
+/*
+#############################################################################
+#
+# Copyright  2024 Chris Anderson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# For a copy of the GNU General Public License see
+# <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+*/
+
+
 //
 //******************************************************************************
 //                          CAMGRAPH.CPP
--- UCclip.h
+++ UCclip.h
@@ -14,7 +14,26 @@
 // A class of static utility functions for implementing "smart" line clipping
 //       
 //********************************************************************************
-//
+/*
+#############################################################################
+#
+# Copyright  2024 Chris Anderson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# For a copy of the GNU General Public License see
+# <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+*/
 #include <cmath>
 
 #ifndef UCclip_
